dbdad881220d82348a554188e0a18419fc1d35a4
Chenuka Sarathchandra <Chenukas@fintrexfinance.com>
2025-08-06 10:21:29 +0530

    🚀 Initial commit: frontend for Fintrex Finny Game

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Questionnaire from './components/Questionnaire'
+
+
+
+function App() {
+  return (
+    <>
+    <Questionnaire/>
+       
+    </>
+  )
+}
+
+export default App
